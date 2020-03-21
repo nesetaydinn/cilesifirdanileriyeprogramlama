797d8829588db37773271a3932e4524686f9b5d3
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-03-21 11:03:45 +0300

    Create intDegiskenTipi.c

---
+++ intDegiskenTipi.c
@@ -0,0 +1,29 @@
+/**<
+*intDegiskenTipi.c
+*Bu program C dilinde kullanılan int Veri Tiplerinin
+*bayt bilgisini konsola bastırır ve
+*programda basit uygulamaları vardır.
+*@authr Neşet Aydın
+*21.03.2020
+*Bu Program ders 5.1.2 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //link gelecek
+*/
+
+#include "stdio.h"
+#include "limits.h"
+
+
+int main(){
+printf("%d\n",sizeof(int));
+printf("%d\n",sizeof(long int));
+printf("%d\n",sizeof(short int));
+printf("%d\n",sizeof(long long int));
+printf("\n\n");
+int sayi =65;
+int karakter ='A';
+
+printf("%c %d\n",sayi,sayi);
+printf("%c %d\n",karakter,karakter);
+
+printf("%c %d\n",sayi+karakter,sayi+karakter);
+return 0;}
