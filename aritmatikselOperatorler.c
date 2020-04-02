89bd4e29713fc218956c09e84926c19d697daf00
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-04-02 20:40:12 +0300

    Create aritmatikselOperatorler.c

---
+++ aritmatikselOperatorler.c
@@ -0,0 +1,51 @@
+/**<
+*aritmatikselOperatorler.c
+*Bu program C dilinde aritmatiksel operatörleri 
+*anlattığımız derste yazılan kodların son halidir.
+*@authr Neşet Aydın
+*2.04.2020
+*Bu Program ders 5.3 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //link
+*/
+
+#include "stdio.h"
+
+/*Aritmatiksel operatörler;
++ toplamayı
+- çıkarmayı
+* çarmayı
+/ bölme
+% mod almayı
+-- bir azaltmayı
+++ bir arttırmayı
+
+*/
+
+
+int main(){
+
+int a= 25;
+int b= 15;
+printf("toplama %d\n",a+b);
+printf("cikarma %d\n",a-b);
+printf("carpma %d\n",a*b);
+printf("bolme %d\n",a/b);
+printf("mod alma %d\n",a%b);
+
+printf("a=%d\n",a);
+a=a+1;
+printf("a=%d\n",a);
+a++;
+printf("a=%d\n",a);
+++a;
+printf("a=%d\n",a);
+
+b=0;
+printf("a=%d, b=%d\n",a,b);
+b=++a;
+printf("a=%d, b=%d\n",a,b);
+b=a++;
+printf("a=%d, b=%d\n",a,b);
+
+
+return 0; }
