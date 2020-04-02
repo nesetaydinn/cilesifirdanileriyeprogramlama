19c1b49a99e8219dc3b147ca67884643e5b48078
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-04-02 20:50:50 +0300

    Update aritmetikselOperatorler.c

--- aritmetikselOperatorler.c
+++ aritmetikselOperatorler.c
@@ -5,7 +5,7 @@
 *@authr Neşet Aydın
 *2.04.2020
 *Bu Program ders 5.3 için yazılan programın son halidir.
-*Videoya linkten ulaşabilirsiniz: //link
+*Videoya linkten ulaşabilirsiniz: //https://youtu.be/DSNRxhublYo
 */
 
 #include "stdio.h"
