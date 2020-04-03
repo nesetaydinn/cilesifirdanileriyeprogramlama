959598412108466f9dc81fca408172a9e5cbdf12
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-04-03 23:53:15 +0300

    Update karsilastirmaOperatorleri.c

--- karsilastirmaOperatorleri.c
+++ karsilastirmaOperatorleri.c
@@ -5,7 +5,7 @@
 *@authr Neşet Aydın
 *3.04.2020
 *Bu Program ders 5.3.1 için yazılan programın son halidir.
-*Videoya linkten ulaşabilirsiniz: //link
+*Videoya linkten ulaşabilirsiniz: //https://youtu.be/5SaYM2X_lUk
 */
 #include "stdio.h"
 
