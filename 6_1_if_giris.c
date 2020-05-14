e0fcdc20d32e99d7b40713346ffd915525bf8ebf
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-05-15 01:32:40 +0300

    Create 6_1_if_giris.c

---
+++ 6_1_if_giris.c
@@ -0,0 +1,30 @@
+/**<
+*6_1_if_giris.c
+*Bu program C dilinde: Program denetim deyimlerinden if'e giriş yaptığımız
+*derste yazılan programdır.
+*@authr Neşet Aydın
+15.05.2020
+*Bu Program ders 6.1 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //link
+*/
+#include <stdio.h>
+/*
+if(kosul) islem;
+    if(kosul){
+
+        işlem1;
+        işlem2;
+    }
+
+*/
+int main()
+{
+    int sayi1, sayi2;
+    printf("Sayi 1;");
+    scanf("%d",&sayi1);
+    printf("Sayi 2;");
+    scanf("%d",&sayi2);
+if (sayi1<sayi2) printf("Sayi1 sayi2 den kucuk ");
+if (sayi1>sayi2) printf("Sayi1 sayi2 den buyuk ");
+    return 0;
+}
