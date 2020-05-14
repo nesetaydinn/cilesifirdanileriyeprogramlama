c49bb1785bec41009e58de962dadc2f6a5b049f6
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-05-15 01:45:51 +0300

    Create 6_1_2_if_else.c

---
+++ 6_1_2_if_else.c
@@ -0,0 +1,24 @@
+  
+/**<
+*6_1_2_if_else.c
+*Bu program C dilinde: Program denetim deyimlerinden if -else if - else 
+*kullanımını anlattığımız
+*derste yazılan programdır.
+*@authr Neşet Aydın
+15.05.2020
+*Bu Program ders 6.1.2 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //https://youtu.be/e4CeyT_A9Ok
+*/
+#include <stdio.h>
+
+int main()
+{
+    if(0) printf("1. if");
+    else if(1) printf("2. if");
+    else if(0) printf("3. if");
+    else if(1) printf("4. if");
+    else if(0) printf("5. if");
+    else printf("Else");
+
+    return 0;
+}
