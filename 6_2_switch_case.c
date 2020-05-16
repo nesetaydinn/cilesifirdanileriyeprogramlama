1136727ea349ab1e8e3159d6566aec795aab21b4
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-05-17 02:31:47 +0300

    Create 6_2_switch_case.c

---
+++ 6_2_switch_case.c
@@ -0,0 +1,27 @@
+/**<
+*6_2_switch_case.c
+*Bu program C dilinde: Program denetim deyimlerinden switch-case 
+*kullanımını anlattığımız
+*derste yazılan programdır.
+*@authr Neşet Aydın
+17.05.2020
+*Bu Program ders 6.2 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //
+*/
+#include <stdio.h>
+
+
+int main()
+{
+int degisken=1;
+printf("%d\n",degisken);
+switch(degisken){
+case 1:
+    printf("case 1\n");
+case 3:
+    printf("case 1\n");
+default:
+    printf("default\n");
+}
+    return 0;
+}
