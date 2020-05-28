0234811e96c9a207037b40deaf1de74151283db7
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-05-28 12:04:48 +0300

    Create 6_8_3_uygulama.c

---
+++ 6_8_3_uygulama.c
@@ -0,0 +1,28 @@
+/**<
+*6_8_3_uygulama.c
+*Bu program C dilinde: Girdiğimiz iki tane tam sayının ortak
+*bölenlerinin en büyüğünü hesaplar.
+*@authr Neşet Aydın
+28.05.2020
+*Bu Program uygulama 6.8.3 için yazılan programın son halidir.
+*Videoya linkten ulaşabilirsiniz: //
+*/
+#include <stdio.h>
+/**
+6.8.3 Klavyeden girilen 2 tam sayının ebob'unu bulan ve konsola bastıran programı yazınız.
+*/
+
+int main()
+{
+
+    int a,b,ebob;
+    printf("ilk degeri giriniz;");
+    scanf("%d",&a);
+    printf("ikinci degeri giriniz;");
+    scanf("%d",&b);
+    for(int sayac=1;sayac<=a && sayac<=b; sayac++){
+        if(a%sayac==0 && b%sayac==0) ebob =sayac;
+    }
+    printf("\nebob=%d",ebob);
+    return 0;
+}
