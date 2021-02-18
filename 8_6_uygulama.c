95bf3c4bd9faeb8895455fa46d399a660dc4b460
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2021-02-18 13:34:06 +0300

    Update 8_6_uygulama.c

--- 8_6_uygulama.c
+++ 8_6_uygulama.c
@@ -5,45 +5,54 @@ küçükten büyüğe sıralıyan fonksiyonun yazılasını içerir.
 *@authr Neşet Aydın
 *08.02.2021
 *Bu Program uygulama 8.6 için yazılan programın son halidir.
-*Algoritmanın dans olarak anlatıldığı video: https://www.youtube.com/watch?v=lyZQPjUT5B4
+*Algoritmanın dans olarak anlatıldığı video: 
 *Videoya linkten ulaşabilirsiniz: 
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 #define boyut 5
-void bastirDizi(int dizi[]);
-void kabarcikSiralama(int dizi[]);
+
+void bastir(int dizi[]);
+void kabarcikSiralamaBuyuktenKucuge(int dizi[]);
+void kabarcikSiralamaKucuktenBuyuge(int dizi[]);
+
 int main()
 {
     int dizi[boyut];
-
-    for (int x=0;x<boyut;x++)dizi[x]=rand()%100;
-    bastirDizi(dizi);
-    kabarcikSiralama(dizi);
-
+    for(int i=0;i<boyut;i++)dizi[i]=rand()%51;
+    bastir(dizi);
+    kabarcikSiralamaBuyuktenKucuge(dizi);
     return 0;
 }
 
-void bastirDizi(int dizi[]){
-    printf("\n\n");
-for (int x=0;x<boyut;x++)printf("%d ",dizi[x]);
-}
-void kabarcikSiralama(int dizi[]){
+void kabarcikSiralamaBuyuktenKucuge(int dizi[]){
 
-int gecici;
 
-for (int x=0;x<boyut-1;x++){
-   for(int y=0;y<boyut-1;y++){
-    if(dizi[y]>dizi[y+1]){
-        gecici=dizi[y];
-        dizi[y]=dizi[y+1];
-        dizi[y+1]=gecici;
-    }
+    for(int i=boyut-1;i>0;i--) //iteraston
+        for(int j=0;j<boyut-1;j++){
+            if(dizi[j]<dizi[j+1]){
+                int temp =dizi[j];
+                dizi[j]=dizi[j+1];
+                dizi[j+1]=temp;
+            }
+        }
+        bastir(dizi);
 }
-printf("iterasyon alindi");
-bastirDizi(dizi);
-}
-printf("Bitti");bastirDizi(dizi);;
 
+void kabarcikSiralamaKucuktenBuyuge(int dizi[]){
+
+
+    for(int i=boyut-1;i>0;i--) //iteraston
+        for(int j=0;j<boyut-1;j++){
+            if(dizi[j]>dizi[j+1]){
+                int temp =dizi[j];
+                dizi[j]=dizi[j+1];
+                dizi[j+1]=temp;
+            }
+        }
+        bastir(dizi);
 }
+void bastir(int dizi[]){
+for(int i=0;i<boyut;i++)printf("%d\t",dizi[i]);
+printf("\n\n");}
